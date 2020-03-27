4ae72f20b8d9c5c8fd19fbf3d87bc0c8662e20b5
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 01:36:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,11 @@ def prime?(n)
    if (n <= 1)
         return False
    end
-    # Check from 2 to n-1 
     for i in range(2, n) 
         if (n % i == 0) 
-            return False
+            return false
    end
-    return True
+    return true
   end
 end
 
