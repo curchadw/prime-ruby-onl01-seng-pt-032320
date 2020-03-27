f761e4f2262723f003bd23c0f7c33a731a2b5e21
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 01:05:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 
 def prime?(number)
-  if Prime.prime?(number)
-    true
-  else
-    false
+  (2..(number - 1)).each do |n|
+    return false if number % n == 0
   end
+  true
 end
