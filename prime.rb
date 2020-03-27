76577f7cccbe824c0fa9f38ad41ea76d16fa088e
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 01:28:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,6 @@
 # Add  code here!
 
 def prime?(num)
-  prime = true
-    for r in -1..Math.sqrt(self).to_i
-      if (self % r == 0)
-        prime = false
-        break
-      end
-    end
-    return prime
-  end
+  
 end
-end
+
