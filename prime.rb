0d9d512e11e1ee21c6a515c2f461c39821811cf2
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-27 01:10:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 # Add  code here!
 
 def prime?(num)
- 
+ unless num.is_a? Integer
+  puts "That's not an integer."
+  if num%2 !=0 && num!=2
+    puts "#{num} is prime!"
+  else
+    puts "#{num} is not prime."
+  end
 end
